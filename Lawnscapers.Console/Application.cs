a0d11fcb4ce4755197cd09c44f1ac60236bbe402
Anthony Kazyaka <ask.anthony@hotmail.com>
2025-01-29 21:31:39 -0500

    Update projects & solution and add console app for cmdline data processing

---
+++ Lawnscapers.Console/Application.cs
@@ -0,0 +1,32 @@
+﻿using Google.Cloud.Firestore;
+using Lawnscapers.Providers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lawnscapers.Console
+{
+    public class Application
+    {
+        private readonly IPlayerProvider _playerProvider;
+
+        public Application(IPlayerProvider playerProvider)
+        {
+            _playerProvider = playerProvider;
+        }
+
+        public async Task RunAsync()
+        {
+            var allPlayers = await _playerProvider.GetAllPlayersAsync();
+
+            foreach (var player in allPlayers)
+            {
+                System.Console.WriteLine($"Player ID: {player.Id}");
+                System.Console.WriteLine(); // Adjust according to your data structure
+            }
+        }
+    }
+
+}
